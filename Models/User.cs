3b36eff2ddcd52e8a68018968399a9769b683282
Kristian Law <grinevich.sergey.official@gmail.com>
2022-04-02 09:32:57 +0300

    Добавьте файлы проекта.

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YumJTest.Models
+{
+    public class User
+    {
+        public int Id;
+        public string FullName;
+        public string Username;
+        public string Phone;
+        public string Website;
+        public string Street;
+        public string Suite;
+        public string City;
+        public int intZipcode;
+    }
+}
